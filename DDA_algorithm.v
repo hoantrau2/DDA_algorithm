84395f2ecfcf91aea91b11edb6576020976869a6
hoantrau2 <hoan.duongmybk852002@hcmut.edu.vn>
2023-10-25 11:19:31 +0700

    flag_T =1ms, clk = 1us, clk1 =100us

--- DDA_algorithm.v
+++ DDA_algorithm.v
@@ -2,14 +2,16 @@ module DDA_algorithm(clk,WR,LS, Nx,N, Pulse,Dir, flag_T);
 	input clk, WR, LS, flag_T;
 	input [7:0] N, Nx;
 	output Pulse, Dir;
+	reg Pulse;
+	
 	reg [7:0] acc;
 	initial acc =N;
 	reg clk1;
 	reg pinout;
 	reg [6:0] counter_clk1;
 	initial counter_clk1 =0;
-	reg Pulse;
 	reg [7:0]temp;
+	
 	assign Dir = Nx[7];
 
 	always @(posedge WR)
@@ -25,19 +27,16 @@ module DDA_algorithm(clk,WR,LS, Nx,N, Pulse,Dir, flag_T);
 			Pulse = clk1 &pinout;
 			end
 			counter_clk1 = counter_clk1 +1;
-			if (counter_clk1 ==50)
-				begin
-					if (clk1 ==1)
-						begin
+			if (counter_clk1 ==50)begin
+					if (clk1 ==1)begin
 			            clk1 =0;
 			            counter_clk1 = 0;
-			            end
-			         else 
-			            begin
+			         end
+			         else begin
 			            clk1 =1;
 			            counter_clk1 = 0;
-			            end
-			    end
+			         end
+			end
 		end
 	
 	always @(clk1)
@@ -45,10 +44,9 @@ module DDA_algorithm(clk,WR,LS, Nx,N, Pulse,Dir, flag_T);
 			if (clk1 == 1) 
 				begin
 				acc = acc + temp;
-				if (acc>N) 
-					begin
+				if (acc>N)begin
 				acc = acc -N; pinout =1;
-					end 
+				end 
 				else pinout =0;
 				end
 			else begin 
